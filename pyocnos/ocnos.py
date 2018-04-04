0869606b72e67b0a34e75904c5e49285fa4b066f
Hassan Khalid <hassan@linx.net>
2018-04-04 14:49:45 +0100

    Command line tool to connect and perform actions on the device

--- pyocnos/ocnos.py
+++ pyocnos/ocnos.py
@@ -7,15 +7,15 @@ from future.utils import raise_from
 from ncclient import NCClientError
 from ncclient import manager
 
-from pyocnos.exceptions import OCNOSConnectionException
-from pyocnos.exceptions import OCNOSNoCandidateConfigException
-from pyocnos.exceptions import OCNOSLoadCandidateConfigFileReadException
-from pyocnos.exceptions import OCNOSCandidateConfigNotLoadedException
-from pyocnos.exceptions import \
-    OCNOSCandidateConfigNotInServerCapabilitiesException
-from pyocnos.exceptions import OCNOSUnableToRetrieveConfigException
-from pyocnos.exceptions import OCNOSUnOpenedConnectionException
 from pyocnos.diff.xml_diff import XmlDiff
+from pyocnos.exceptions import OCNOSCandidateConfigInvalidError
+from pyocnos.exceptions import OCNOSCandidateConfigNotInServerCapabilitiesError
+from pyocnos.exceptions import OCNOSCandidateConfigNotLoadedError
+from pyocnos.exceptions import OCNOSConnectionError
+from pyocnos.exceptions import OCNOSLoadCandidateConfigFileReadError
+from pyocnos.exceptions import OCNOSNoCandidateConfigError
+from pyocnos.exceptions import OCNOSUnOpenedConnectionError
+from pyocnos.exceptions import OCNOSUnableToRetrieveConfigError
 
 
 class OCNOS(object):
@@ -69,7 +69,7 @@ class OCNOS(object):
         Open a connection to an OcNOS running device using SSH.
 
         Returns: None
-        Raises: OCNOSConnectionException
+        Raises: OCNOSConnectionError
         """
 
         try:
@@ -84,7 +84,7 @@ class OCNOS(object):
         except NCClientError as ncclient_exception:
             self.log.error('Error', exc_info=True)
             raise_from(
-                OCNOSConnectionException('Unable to open ssh connection.'),
+                OCNOSConnectionError('Unable to open ssh connection.'),
                 ncclient_exception
             )
         else:
@@ -99,7 +99,7 @@ class OCNOS(object):
         Close the SSH connection to the OcNOS running device.
 
         Returns: None
-        Raises: OCNOSConnectionException
+        Raises: OCNOSConnectionError
 
         """
         if self._connection:
@@ -108,7 +108,7 @@ class OCNOS(object):
             except NCClientError as ncclient_exception:
                 self.log.error('error', exc_info=True)
                 raise_from(
-                    OCNOSConnectionException(
+                    OCNOSConnectionError(
                         'Unable to close ssh connection.'
                     ),
                     ncclient_exception
@@ -130,32 +130,6 @@ class OCNOS(object):
         """
         return self._connection.connected if self._connection else False
 
-    def get_running_config(self):
-        """
-        Populate running_config from remote device.
-
-        Returns: String
-        Raises: OCNOSUnOpenedConnectionException,
-         OCNOSUnableToRetrieveConfigException
-
-        """
-        if self._connection:
-            try:
-                config = self._connection.get_config(source='running').data_xml
-            except NCClientError as ncclient_exception:
-                self.log.error('Error', exc_info=True)
-                raise_from(
-                    OCNOSUnableToRetrieveConfigException(),
-                    ncclient_exception
-                )
-            else:
-                running_config = lxml.etree.fromstring(config.encode())
-                running_config.tag = 'config'
-                return lxml.etree.tostring(running_config, encoding='UTF-8', pretty_print=True)
-        else:
-            self.log.error('Error', exc_info=True)
-            raise OCNOSUnOpenedConnectionException()
-
     def load_candidate_config(self, filename=None, config=None):
         """
         Load candidate_config from a string or file like object
@@ -166,17 +140,17 @@ class OCNOS(object):
                           Default: None.
 
         Returns: None
-        Raises: OCNOSLoadCandidateConfigException,
-         OCNOSLoadCandidateConfigFileReadException
+        Raises: OCNOSLoadCandidateConfigError,
+         OCNOSLoadCandidateConfigFileReadError
 
         """
         if filename is None and config is None:
-            raise OCNOSNoCandidateConfigException
+            raise OCNOSNoCandidateConfigError
         elif filename:
             try:
                 self._candidate_config = lxml.etree.parse(filename).getroot()
             except IOError as io_error:
-                raise_from(OCNOSLoadCandidateConfigFileReadException, io_error)
+                raise_from(OCNOSLoadCandidateConfigFileReadError, io_error)
         else:
             self._candidate_config = lxml.etree.fromstring(config)
         self._candidate_config.tag = 'config'
@@ -186,20 +160,21 @@ class OCNOS(object):
         """
         Commit the loaded candidate config
         Returns: None
-        Raises: OCNOSUnOpenedConnectionException
-            OCNOSCandidateConfigNotLoadedException
-            OCNOSCandidateConfigNotInServerCapabilitiesException
+        Raises: OCNOSUnOpenedConnectionError
+            OCNOSCandidateConfigNotLoadedError
+            OCNOSCandidateConfigNotInServerCapabilitiesError
+            OCNOSCandidateConfigInvalidError
         """
         if self._candidate_config is None:
             self.log.error('Error: Candidate config not loaded')
-            raise OCNOSCandidateConfigNotLoadedException
+            raise OCNOSCandidateConfigNotLoadedError
 
         if not self._connection:
             self.log.error('Error: no open connection', exc_info=True)
-            raise OCNOSUnOpenedConnectionException()
+            raise OCNOSUnOpenedConnectionError
 
         if ':candidate' not in self._connection.server_capabilities:
-            raise OCNOSCandidateConfigNotInServerCapabilitiesException
+            raise OCNOSCandidateConfigNotInServerCapabilitiesError
 
         # discard old candidate config
         # lock the current candidate config
@@ -208,12 +183,19 @@ class OCNOS(object):
 
         self._connection.discard_changes()
         with self._connection.locked(target='candidate'):
-            self._connection.edit_config(
-                target='candidate',
-                config=self._candidate_config,
-                test_option='test-then-set',
-                default_operation='replace'
-            )
+            try:
+                self._connection.edit_config(
+                    target='candidate',
+                    config=self._candidate_config,
+                    test_option='test-then-set',
+                    default_operation='replace'
+                )
+            except NCClientError as ncclient_exception:
+                self.log.error('error', exc_info=True)
+                raise_from(
+                    OCNOSCandidateConfigInvalidError,
+                    ncclient_exception
+                )
             self._connection.commit()
         self._connection.copy_config(source='running', target='startup')
 
@@ -221,15 +203,71 @@ class OCNOS(object):
         """
         Diff on the running and candidate config
         Returns: List
+        Raises: OCNOSCandidateConfigNotLoadedError
+            OCNOSUnOpenedConnectionError
 
         """
         if self._candidate_config is None:
             self.log.error('Error: Candidate config not loaded')
-            raise OCNOSCandidateConfigNotLoadedException
+            raise OCNOSCandidateConfigNotLoadedError
 
         if not self._connection:
             self.log.error('Error: no open connection', exc_info=True)
-            raise OCNOSUnOpenedConnectionException()
+            raise OCNOSUnOpenedConnectionError
 
-        xml_diff = XmlDiff(self.get_running_config(), lxml.etree.tostring(self._candidate_config))
-        return xml_diff.get_printable_diff()
+        xml_diff = XmlDiff(self._get_config_from_device('running'), lxml.etree.tostring(self._candidate_config))
+        return xml_diff.get_diff_string()
+
+    def _get_config_from_device(self, config_name):
+        """
+        Get config from device depending on config name
+        Args:
+            config_name: (String) e.g. running or startup
+
+        Returns: (String) xml string representing the config
+        Raises: OCNOSUnOpenedConnectionError,
+         OCNOSUnableToRetrieveConfigError
+
+        """
+        if self._connection:
+            try:
+                config = self._connection.get_config(source=config_name).data_xml
+            except NCClientError as ncclient_exception:
+                self.log.error('Error', exc_info=True)
+                raise_from(
+                    OCNOSUnableToRetrieveConfigError,
+                    ncclient_exception
+                )
+            else:
+                running_config = lxml.etree.fromstring(config.encode())
+                running_config.tag = 'config'
+                return lxml.etree.tostring(running_config, encoding='UTF-8', pretty_print=True)
+        else:
+            self.log.error('Error', exc_info=True)
+            raise OCNOSUnOpenedConnectionError
+
+    def get_config(self, retrieve='all'):
+        """
+        Get all or a specific config
+        Args:
+            retrieve: (String) could be all or one of startup, running,
+              or candidate
+
+        Returns: Dict
+
+        """
+        return {
+            'startup': self._get_config_from_device('startup') if retrieve in ('startup', 'all') else '',
+            'running': self._get_config_from_device('running') if retrieve in ('running', 'all') else '',
+            'candidate': self._get_config_from_device('candidate') if retrieve in ('candidate', 'all') else ''
+        }
+
+    def discard_config(self):
+        """
+        Clear previously loaded candidate_config without committing it.
+
+        Returns: None
+        """
+
+        self._candidate_config = None
+        self.log.info("candidate_config discarded")
--- test/test_ocnos.py
+++ test/test_ocnos.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 import unittest
 
@@ -5,14 +6,14 @@ import lxml
 import mock
 from ncclient import NCClientError
 
-from pyocnos.exceptions import \
-    OCNOSCandidateConfigNotInServerCapabilitiesException
-from pyocnos.exceptions import OCNOSCandidateConfigNotLoadedException
-from pyocnos.exceptions import OCNOSConnectionException
-from pyocnos.exceptions import OCNOSLoadCandidateConfigFileReadException
-from pyocnos.exceptions import OCNOSNoCandidateConfigException
-from pyocnos.exceptions import OCNOSUnOpenedConnectionException
-from pyocnos.exceptions import OCNOSUnableToRetrieveConfigException
+from pyocnos.exceptions import OCNOSCandidateConfigInvalidError
+from pyocnos.exceptions import OCNOSCandidateConfigNotInServerCapabilitiesError
+from pyocnos.exceptions import OCNOSCandidateConfigNotLoadedError
+from pyocnos.exceptions import OCNOSConnectionError
+from pyocnos.exceptions import OCNOSLoadCandidateConfigFileReadError
+from pyocnos.exceptions import OCNOSNoCandidateConfigError
+from pyocnos.exceptions import OCNOSUnOpenedConnectionError
+from pyocnos.exceptions import OCNOSUnableToRetrieveConfigError
 from pyocnos.ocnos import OCNOS
 
 connect_path = 'pyocnos.ocnos.manager.connect'
@@ -46,7 +47,7 @@ class TestOCNOS(unittest.TestCase):
     @mock.patch(connect_path)
     def test_fail_open_when_ncclient_raises_exception(self, mock_manager_connect):
         mock_manager_connect.side_effect = NCClientError
-        self.assertRaises(OCNOSConnectionException, self.device.open)
+        self.assertRaises(OCNOSConnectionError, self.device.open)
 
     @mock.patch(connect_path)
     def test_success_close(self, mock_manager_connect):
@@ -59,7 +60,7 @@ class TestOCNOS(unittest.TestCase):
     def test_fail_close_when_ncclient_raises_exception(self, mock_manager_connect):
         self.device.open()
         self.device._connection.close_session.side_effect = NCClientError
-        self.assertRaises(OCNOSConnectionException, self.device.close)
+        self.assertRaises(OCNOSConnectionError, self.device.close)
 
     def test_is_alive_when_open_not_called(self):
         self.assertFalse(self.device.is_alive())
@@ -70,29 +71,29 @@ class TestOCNOS(unittest.TestCase):
         self.device._connection.connected = True
         self.assertTrue(self.device.is_alive())
 
-    def test_success_get_running_config(self):
+    def test_success_get_config_for_running(self):
         with mock.patch(connect_path) as mock_manager_connect:
             get_config_mock = mock_manager_connect.return_value.get_config.return_value = mock.MagicMock()
             get_config_mock.data_xml = '<vr>data</vr>'
             self.device.open()
-            self.assertIsNotNone(self.device.get_running_config())
+            self.assertIsNotNone(self.device.get_config('running'))
 
-    def test_fail_get_running_config_when_no_connection(self):
-        self.assertRaises(OCNOSUnOpenedConnectionException, self.device.get_running_config)
+    def test_fail_get_config_when_no_connection(self):
+        self.assertRaises(OCNOSUnOpenedConnectionError, self.device.get_config)
 
-    def test_fail_get_running_config_when_ncclinet_raises_exception(self):
+    def test_fail_get_config_when_ncclinet_raises_exception(self):
         with mock.patch(connect_path) as mock_manager_connect:
             self.device.open()
             get_config_mock = mock_manager_connect.return_value.get_config = mock.MagicMock()
             get_config_mock.side_effect = NCClientError
-            self.assertRaises(OCNOSUnableToRetrieveConfigException,
-                              self.device.get_running_config)
+            self.assertRaises(OCNOSUnableToRetrieveConfigError,
+                              self.device.get_config)
 
     def test_fail_load_candidate_config_when_nor_string_or_file_given(self):
-        self.assertRaises(OCNOSNoCandidateConfigException, self.device.load_candidate_config)
+        self.assertRaises(OCNOSNoCandidateConfigError, self.device.load_candidate_config)
 
     def test_fail_load_candidate_config_when_unable_to_read_file(self):
-        with self.assertRaises(OCNOSLoadCandidateConfigFileReadException):
+        with self.assertRaises(OCNOSLoadCandidateConfigFileReadError):
             self.device.load_candidate_config(filename='foo.bar')
 
     def test_success_load_candidate_config_with_file(self):
@@ -113,19 +114,38 @@ class TestOCNOS(unittest.TestCase):
         )
 
     def test_fail_commit_config_when_no_candidate_config_loaded(self):
-        self.assertRaises(OCNOSCandidateConfigNotLoadedException, self.device.commit_config)
+        self.assertRaises(OCNOSCandidateConfigNotLoadedError, self.device.commit_config)
 
     def test_fail_commit_config_when_no_open_connection(self):
         self.device.load_candidate_config(config='<config>foo</config>')
-        self.assertRaises(OCNOSUnOpenedConnectionException, self.device.commit_config)
+        self.assertRaises(OCNOSUnOpenedConnectionError, self.device.commit_config)
 
     def test_fail_commit_config_when_candidate_not_in_server_capabilities(self):
         with mock.patch(connect_path) as mock_manager_connect:
             self.device.open()
             self.device.load_candidate_config(config='<config></config>')
             self.device._connection.server_capabilities = [':validate:1.0']
-            self.assertRaises(OCNOSCandidateConfigNotInServerCapabilitiesException, self.device.commit_config)
+            self.assertRaises(OCNOSCandidateConfigNotInServerCapabilitiesError, self.device.commit_config)
             self.device.close()
+
+    def test_fail_commit_config_when_ncclinet_raises_exception(self):
+        config = '<config></config>'
+        with mock.patch(connect_path) as mock_manager_connect:
+            instance = mock_manager_connect.return_value
+            instance.edit_config.side_effect = NCClientError
+            self.device.open()
+            self.device.load_candidate_config(config=config)
+            self.device._connection.server_capabilities = [
+                ':candidate',
+                ':validate:1.0'
+            ]
+            self.assertRaises(OCNOSCandidateConfigInvalidError, self.device.commit_config)
+            self.device.close()
+        instance.discard_changes.assert_called_once()
+        instance.locked.assert_called_with(target='candidate')
+
+        instance.commit.assert_not_called()
+        instance.copy_config.assert_not_called()
 
     def test_success_commit_config(self):
         config = '<config></config>'
@@ -154,11 +174,11 @@ class TestOCNOS(unittest.TestCase):
         )
 
     def test_fail_compare_config_when_no_candidate_config_loaded(self):
-        self.assertRaises(OCNOSCandidateConfigNotLoadedException, self.device.compare_config)
+        self.assertRaises(OCNOSCandidateConfigNotLoadedError, self.device.compare_config)
 
     def test_fail_compare_config_when_no_open_connection(self):
         self.device.load_candidate_config(config='<config>foo</config>')
-        self.assertRaises(OCNOSUnOpenedConnectionException, self.device.compare_config)
+        self.assertRaises(OCNOSUnOpenedConnectionError, self.device.compare_config)
 
     def test_success_compare_config(self):
         with mock.patch(connect_path) as mock_manager_connect:
@@ -167,4 +187,60 @@ class TestOCNOS(unittest.TestCase):
             self.device.open()
             self.device.load_candidate_config(config='<config><vr><vrf>2</vrf></vr></config>')
             expected = ['[vr]', '- <vrf>1</vrf>', '+ <vrf>2</vrf>']
-            self.assertEqual(expected, self.device.compare_config())
+            self.assertEqual('{}'.format(os.linesep).join(expected), self.device.compare_config())
+
+    def test_success_get_config_for_startup(self):
+        with mock.patch(connect_path) as mock_manager_connect:
+            get_config_mock = mock_manager_connect.return_value.get_config.return_value = mock.MagicMock()
+            get_config_mock.data_xml = '<vr>1</vr>'
+            self.device.open()
+
+            config_element = lxml.etree.Element('config')
+            config_element.text = '1'
+            expected = lxml.etree.tostring(config_element, encoding='UTF-8', pretty_print=True)
+            self.assertEqual(expected, self.device.get_config('startup')['startup'])
+
+    def test_success_get_config_for_all_option(self):
+        with mock.patch(connect_path) as mock_manager_connect:
+            get_config_mock = mock_manager_connect.return_value.get_config.return_value = mock.MagicMock()
+            get_config_mock.data_xml = '<data><vr>1</vr></data>'
+            self.device.open()
+            config_element = lxml.etree.Element('config')
+            vr = lxml.etree.SubElement(config_element, 'vr')
+            vr.text = '1'
+            config_str = lxml.etree.tostring(config_element, encoding='UTF-8', pretty_print=True)
+            expected = {
+                'running': config_str,
+                'startup': config_str,
+                'candidate': config_str
+            }
+            self.assertEqual(
+                expected,
+                self.device.get_config()
+            )
+
+    def test_success_get_config_for_running(self):
+        with mock.patch(connect_path) as mock_manager_connect:
+            get_config_mock = mock_manager_connect.return_value.get_config.return_value = mock.MagicMock()
+            get_config_mock.data_xml = '<data><vr>1</vr></data>'
+            self.device.open()
+            config_element = lxml.etree.Element('config')
+            vr = lxml.etree.SubElement(config_element, 'vr')
+            vr.text = '1'
+            config_str = lxml.etree.tostring(config_element, encoding='UTF-8', pretty_print=True)
+            expected = {
+                'running': config_str,
+                'startup': '',
+                'candidate': ''
+            }
+            self.assertEqual(
+                expected,
+                self.device.get_config(retrieve='running')
+            )
+
+    def test_success_discard_config(self):
+        self.device.load_candidate_config(config='<config>foo</config>')
+        self.assertEqual(b'<config>foo</config>', lxml.etree.tostring(self.device._candidate_config))
+
+        self.device.discard_config()
+        self.assertIsNone(self.device._candidate_config)
