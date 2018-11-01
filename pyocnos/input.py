49f6f3b04ec3affe892e4b44b3811fa6791c6abd
Jim Wright <jim@linx.net>
2018-11-01 11:13:43 +0000

    [SOFT-4668] Use builtin strtobool

--- pyocnos/input.py
+++ pyocnos/input.py
@@ -1,35 +1,13 @@
+from distutils.util import strtobool
 import sys
 
 
-def query_yes_no(question, default="yes"):
-    """From http://code.activestate.com/recipes/577058/
-    Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
+def query_yes_no(question):
     while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
+        try:
+            choice = strtobool(raw_input(question))
+        except ValueError:
             sys.stdout.write("Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n').\n")
+            continue
+        return bool(choice)
